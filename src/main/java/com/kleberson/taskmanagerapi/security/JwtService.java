7bcabd4ce96faae2ab5a23004cd1326005016941
kleberson154 <kleberson55@hotmail.com>
2026-01-06 01:15:14 -0300

    feat: add JwtService for JWT token generation

---
+++ src/main/java/com/kleberson/taskmanagerapi/security/JwtService.java
@@ -0,0 +1,41 @@
+package com.kleberson.taskmanagerapi.security;
+
+import com.kleberson.taskmanagerapi.model.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class JwtService {
+    @Value("${spring.security.jwt.secret}")
+    private String secret;
+
+    @Value("${spring.security.jwt.expiration}")
+    private long expiration;
+
+    public String generateToken(User user) {
+        return Jwts.builder()
+                .setSubject(user.getEmail())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes()), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(secret.getBytes())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean isTokenValid(String token, User user) {
+        return extractEmail(token).equals(user.getEmail());
+    }
+}
