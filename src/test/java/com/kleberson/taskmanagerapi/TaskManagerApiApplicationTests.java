d8d1e2e43d4387a1322addc6a9aafbed5caa7881
kleberson154 <kleberson55@hotmail.com>
2026-01-06 14:34:06 -0300

    fix: update test task manager

--- src/test/java/com/kleberson/taskmanagerapi/TaskManagerApiApplicationTests.java
+++ src/test/java/com/kleberson/taskmanagerapi/TaskManagerApiApplicationTests.java
@@ -1,19 +1,16 @@
 package com.kleberson.taskmanagerapi;
 
-import com.kleberson.taskmanagerapi.security.JwtAuthFilter;
+import com.kleberson.taskmanagerapi.config.TestSecurityConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
+
 class TaskManagerApiApplicationTests {
-
-    @MockBean
-    JwtAuthFilter jwtAuthFilter;
-
-
     @Test
     void contextLoads() {
     }
