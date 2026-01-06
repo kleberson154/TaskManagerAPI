4e4cfdd13e29b2b62b88d05e1d0dc4034dbcb6ba
kleberson154 <kleberson55@hotmail.com>
2026-01-06 14:29:28 -0300

    fix: remove TestSecurityConfig import

--- src/test/java/com/kleberson/taskmanagerapi/TaskManagerApiApplicationTests.java
+++ src/test/java/com/kleberson/taskmanagerapi/TaskManagerApiApplicationTests.java
@@ -1,16 +1,13 @@
 package com.kleberson.taskmanagerapi;
 
-import com.kleberson.taskmanagerapi.config.TestSecurityConfig;
 import com.kleberson.taskmanagerapi.security.JwtAuthFilter;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-@Import(TestSecurityConfig.class)
 class TaskManagerApiApplicationTests {
 
     @MockBean
