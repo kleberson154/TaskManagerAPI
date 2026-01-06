44e6b942e66028e50c54ca7f9ceab6b782956786
kleberson154 <kleberson55@hotmail.com>
2026-01-06 01:03:03 -0300

    feat: create User entity

---
+++ src/main/java/com/kleberson/taskmanagerapi/model/User.java
@@ -0,0 +1,32 @@
+package com.kleberson.taskmanagerapi.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "user")
+    private List<Task> tasks;
+}
